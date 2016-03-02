27cef21113609026a9fc437470fca02ae220fe55
Brian Orozco <ssjbrian@gmail.com>
2016-03-02 16:54:30 -0500

    Added files via upload

---
+++ HW02_BrianOrozco.py
@@ -0,0 +1,136 @@
+'''
+Brian Orozco
+CS 100 H01
+September 13th, 2015
+
+Homework 02
+'''
+
+import turtle
+
+#CS.2
+#square
+s = turtle.Screen()
+t = turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+s.bye()
+
+#CS.3
+#diamond
+s = turtle.Screen()
+t = turtle.Turtle()
+t.left(45)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+s.bye()
+
+#CS.4
+#pentagon
+s = turtle.Screen()
+t = turtle.Turtle()
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+s.bye()
+
+#hexagon
+s = turtle.Screen()
+t = turtle.Turtle()
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+s.bye()
+
+#heptagon
+s = turtle.Screen()
+t = turtle.Turtle()
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+t.forward(100)
+t.left(51 + 3/7)
+s.bye()
+
+#octogan
+s = turtle.Screen()
+t = turtle.Turtle()
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+s.bye()
+
+#CS.6
+#four concentric circles
+s = turtle.Screen()
+t = turtle.Turtle()
+t.circle(25)
+t.penup()
+t.right(90)
+t.forward(25)
+t.left(90)
+t.pendown()
+t.circle(50)
+t.penup()
+t.right(90)
+t.forward(25)
+t.left(90)
+t.pendown()
+t.circle(75)
+t.penup()
+t.right(90)
+t.forward(25)
+t.left(90)
+t.pendown()
+t.circle(100)
+s.bye()
